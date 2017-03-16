8eb9066536d4b9406800c8f9929e3e8c8a675e1f
Juliusb <Juliusb@ZYLUN-PC-0308>
2017-03-17 03:53:33 +0800

    menu toggling complete

--- Framework/ext-modules/psMenu/psMenuController.js
+++ Framework/ext-modules/psMenu/psMenuController.js
@@ -25,12 +25,37 @@ angular.module('psMenu').controller('psMenuController',
                     { route: route });
             };
 
+            this.setOpenMenuScope = function (scope) {
+                $scope.openMenuScope = scope;
+            };
+
             $scope.toggleMenuOrientation = function () {
+
+                //close any open menu
+                if ($scope.openMenuScope)
+                    $scope.openMenuScope.closeMenu();
+
                 $scope.isVertical = !$scope.isVertical;
 
                 $rootScope.$broadcast('ps-menu-orientation-change-event',
                     { isMenuVertical: $scope.isVertical });
             };
+
+            // this is binding to jquery click
+            angular.element(document).bind('click', function (e) {
+                if ($scope.openMenuScope && !$scope.isVertical) {
+                    if ($(e.target).parent().hasClass('ps-selectable-item'))
+                        return;
+
+                    //must apply if bind from jquery
+                    $scope.$apply(function () {
+                        $scope.openMenuScope.closeMenu();
+                    });
+                    e.preventDefault();
+                    e.stopPropagation();
+
+                }
+            });
 
             $scope.$on('ps-menu-show', function (evt, data) {
                 $scope.showMenu = data.show;
