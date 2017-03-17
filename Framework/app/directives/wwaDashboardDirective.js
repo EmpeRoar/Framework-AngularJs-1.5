82ae321acd4ddae41e71be45d513f18a0cfa8fe3
Juliusb <Juliusb@ZYLUN-PC-0308>
2017-03-18 04:42:17 +0800

    dashboard implementation

--- Framework/app/directives/wwaDashboardDirective.js
+++ Framework/app/directives/wwaDashboardDirective.js
@@ -5,6 +5,36 @@ angular.module('app').directive('wwaDashboard', [function () {
         scope: {
 
         },
-        template: '<h1>Dashboard Page</h1>'
+        template: '<ps-dashboard></ps-dashboard>',
+        link: function (scope) {
+
+            scope.title = "Power Dashboard";
+
+            scope.gridsterOpts = {
+                columns: 12,
+                margins: [20, 20],
+                outerMargin: false,
+                pushing: true,
+                floating: true,
+                swapping: false
+            };
+
+            scope.widgets = [
+                {
+                    title: 'First',
+                    sizeX: 3,
+                    sizeY: 3,
+                    row: 0,
+                    col: 0
+                },
+                {
+                    title: 'Second',
+                    sizeX: 2,
+                    sizeY: 4,
+                    row: 0,
+                    col: 5
+                }
+            ];
+        }
     }
 }]);
